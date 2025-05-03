79880c827ffccf7a4d66f7ab9f4d44f3e1e30827
Oleksha <deryabino@gmail.com>
2025-05-03 11:01:23 +0400

    Урок 4. Валидация

--- app/Livewire/User/UserList.php
+++ app/Livewire/User/UserList.php
@@ -4,25 +4,46 @@ namespace App\Livewire\User;
 
 use App\Models\User;
 use Illuminate\View\View;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class UserList extends Component
 {
+    #[Validate('required', message: 'Имя пользователя обязательно для заполнения')]
+    #[Validate('min:2', message: 'Имя пользователя должно быть длиннее 1 символа')]
+    #[Validate('max:30', message: 'Имя пользователя должно быть не больше 30 символов')]
     public string $name;
+    #[Validate('required|email|max:30')]
     public string $email;
+    #[Validate('required|min:6')]
     public string $password;
 
-    public function addUser()
+    protected function rules(): array
     {
-        User::create([
-            'name' => $this->name,
-            'email' => $this->email,
-            'password' => $this->password,
-        ]);
-        $this->reset('name', 'email', 'password');
+        return [
+            'name' => 'required|min:2|max:30',
+            'email' => 'required|email|max:30',
+            'password' => 'required|min:6',
+        ];
     }
 
-    public function deleteUser(int $id)
+    public function save(): void
+    {
+        $validated = $this->validate();
+        User::create($validated);
+        $this->reset();
+    }
+
+    protected function messages()
+    {
+        return [
+            'name.required' => 'Имя пользователя обязательно для заполнения',
+            'name.min' => 'Имя пользователя должно быть длиннее 1 символа',
+            'name.max' => 'Имя пользователя должно быть не больше 30 символов',
+        ];
+    }
+
+    public function delete(int $id): void
     {
         User::find($id)->delete();
     }
--- resources/views/livewire/user/user-list.blade.php
+++ resources/views/livewire/user/user-list.blade.php
@@ -1,33 +1,30 @@
 <div class="row">
     <div class="col-md-6">
-        <form wire:submit="addUser">
+        <form wire:submit="save">
             <div class="mb-3">
-                <input type="text" name="name" class="form-control" wire:model="name" placeholder="Имя пользователя">
+                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" wire:model.blur="name" placeholder="Имя пользователя">
+                @error('name')<div class="invalid-feedback">{{ $message }}</div>@enderror
             </div>
             <div class="mb-3">
-                <input type="email" class="form-control" wire:model="email" placeholder="Электронная почта">
+                <input type="email" class="form-control @error('email') is-invalid @enderror" wire:model.blur="email" placeholder="Электронная почта">
+                @error('email')<div class="invalid-feedback">{{ $message }}</div>@enderror
             </div>
             <div class="mb-3">
-                <input type="password" class="form-control" wire:model="password" placeholder="Введите пароль">
+                <input type="password" class="form-control @error('password') is-invalid @enderror" wire:model.blur="password" placeholder="Введите пароль">
+                @error('password')<div class="invalid-feedback">{{ $message }}</div>@enderror
             </div>
             <div class="d-flex align-items-center gap-3">
                 <button type="submit" class="btn btn-primary my-2">Добавить пользователя</button>
-                <div wire:loading wire:target="addUser" class="spinner-border" role="status">
+                <div wire:loading wire:target="save" class="spinner-border" role="status">
                     <span class="visually-hidden">Сохранение...</span>
                 </div>
             </div>
         </form>
     </div>
     <div class="col-md-6">
-        <div class="d-flex align-items-center gap-3">
-            <button wire:click="$refresh" type="button" class="btn btn-success mb-2">Обновить данные</button>
-            <div wire:loading class="spinner-border" role="status">
-                <span class="visually-hidden">Обновление...</span>
-            </div>
-        </div>
         <ul>
             @forelse($users as $user)
-                <li wire:key="{{ $user->id }}">{{ $user->name }} ({{ $user->email }}) | <a href="#" wire:click.prevent="deleteUser({{ $user->id }})" wire:confirm="Вы уверены?">Удалить</a></li>
+                <li wire:key="{{ $user->id }}">{{ $user->name }} ({{ $user->email }}) | <a href="#" wire:click.prevent="delete({{ $user->id }})" wire:confirm="Вы уверены?">Удалить</a></li>
             @empty
                 <p>Список пользователей пуст...</p>
             @endforelse
